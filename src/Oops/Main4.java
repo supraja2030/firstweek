3ce86916c702e91726d2a8a645a1370b3f471297
supraja2030 <bsupraja7891@gmail.com>
2024-06-22 20:10:32 +0530

    constructor

---
+++ src/Oops/Main4.java
@@ -0,0 +1,9 @@
+package Oops;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Vehicle4 vehicle1 = new Vehicle4("Car","generic engine", 40000);
+        Vehicle4 vehicle2 = new Vehicle4("Corolla","V6", 60000);
+
+    }
+}
---
+++ src/Oops/Vehicle4.java
@@ -0,0 +1,7 @@
+package Oops;
+
+public class Vehicle4 {
+    public Vehicle4(String name, String engine , double price){
+        System.out.println(name + " has  " + engine + " and the basic price is " + price + " dollers");
+    }
+}
